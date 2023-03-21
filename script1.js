c485f545c8e9cacbf2574d821ebdee4a53e5196a
ferrynandi <42517981+ferrynandi@users.noreply.github.com>
2023-03-21 15:52:49 +0700

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,15 @@
+function popupku() {
+
+  var body = document.querySelector("body");      
+
+  var pWm = document.createElement("a");
+
+  pWm.setAttribute("href", "https://google.com");
+
+  pWm.setAttribute("style", "text-decoration: none; color: white; opacity: .5; position: fixed; bottom : 10px; left: 10px");
+
+  pWm.innerHTML = "Di edit ama Ferry";
+
+  body.appendChild(pWm);
+
+}
