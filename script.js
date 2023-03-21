582c797cf97d8e4950339ea6291723d1159ea9ec
ferrynandi <42517981+ferrynandi@users.noreply.github.com>
2023-03-21 15:00:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function popupku() {
 
   var pWm = document.createElement("a");
 
-  pWm.setAttribute("href", "https://youtube.com/dekatutorial");
+  pWm.setAttribute("href", "");
 
   pWm.setAttribute("style", "text-decoration: none; color: white; opacity: .5; position: fixed; bottom : 10px; left: 10px");
 
